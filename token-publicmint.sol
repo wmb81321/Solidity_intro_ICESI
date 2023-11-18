f4023b3cfb984b3566a6ea3f975bbf229d20c019
William Martinez <68308482+wmb81321@users.noreply.github.com>
2023-11-17 22:29:09 -0500

    Update token-publicmint.sol
    Se genera que sea de mint publica

--- token-publicmint.sol
+++ token-publicmint.sol
@@ -5,17 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @custom:security-contact wmb81321@outlook.com
 contract Cali is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC20("cali", "cali")
-        Ownable(initialOwner)
-    {
+    constructor() ERC20("Cali", "CALI") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
-
-} 
+}
