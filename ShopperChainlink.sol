1b4b413b677819c1f54a2c3318fe30d13bf69889
William Martinez <68308482+wmb81321@users.noreply.github.com>
2023-11-17 22:29:20 -0500

    Update ShopperChainlink.sol
    Se hace menos abstracto

--- ShopperChainlink.sol
+++ ShopperChainlink.sol
@@ -1,65 +1,49 @@
-/SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 interface TokenInterface {
-        function mint(address account, uint256 amount) external returns (bool);
+    function mint(address account, uint256 amount) external;
 }
 
 contract TokenShop {
-    
-    AggregatorV3Interface internal priceFeed; **** toma los precios de un Feed dados por Chainlink***
+    AggregatorV3Interface internal priceFeed;
     TokenInterface public minter;
-    uint256 public tokenPrice = 10000; //1 token = 10 usd, with 2 decimal places
+    uint256 public tokenPrice = 10000; // 1 token = 10 USD, with 2 decimal places
     address public owner;
-    
+
     constructor(address tokenAddress) {
         minter = TokenInterface(tokenAddress);
-        /**
-        * Network: Sepolia
-        * Aggregator: ETH/USD
-        * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        */
         priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         owner = msg.sender;
     }
 
-    /**
-    * Returns the latest price
-    */
     function getLatestPrice() public view returns (int) {
-        (
-            /*uint80 roundID*/,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
+        (,int price,,,) = priceFeed.latestRoundData();
         return price;
     }
 
     function tokenAmount(uint256 amountETH) public view returns (uint256) {
-        //Sent amountETH, how many usd I have
         uint256 ethUsd = uint256(getLatestPrice());
-        uint256 amountUSD = amountETH * ethUsd / 1000000000000000000; //ETH = 18 decimal places
-        uint256 amountToken = amountUSD / tokenPrice / 100;  //2 decimal places
+        uint256 amountUSD = amountETH * ethUsd / 1e18; // ETH tiene 18 decimales
+        uint256 amountToken = amountUSD * 100 / tokenPrice; // 2 decimales para el precio del token
         return amountToken;
-    } 
-        
+    }
+
     receive() external payable {
         uint256 amountToken = tokenAmount(msg.value);
         minter.mint(msg.sender, amountToken);
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Solo el propietario puede ejecutar esta funcion");
         _;
     }
 
     function withdraw() external onlyOwner {
         payable(owner).transfer(address(this).balance);
     }
-    
 }
+
 
