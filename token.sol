35a0ce3f5fd6f965926b97d143a88cfa1925b51b
William Martinez <68308482+wmb81321@users.noreply.github.com>
2023-11-17 21:39:04 -0500

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact wmb81321@outlook.com
+contract Cali is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("cali", "cali")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+} 
